ca129a29413032dfca303f9b52d409c5f1833b10
AhmetSerhan <159904294+AhmetSerhan25@users.noreply.github.com>
2025-10-10 19:28:25 +0300

    Update CardGame.java
    Edited

--- CardGame.java
+++ CardGame.java
@@ -4,106 +4,114 @@ import java.util.ArrayList;
 
 // Cardgame
 // author: Esma Karakaya
-// date: 
-public class CardGame
-{
+// date: 10.10.2025
+public class CardGame {
     // properties
-    Cards             fullPack;
+    Cards fullPack;
     ArrayList<Player> players;
-    ScoreCard         scoreCard;
-    Cards[]           cardsOnTable;
-    int               roundNo;
-    int               turnOfPlayer;
-    
+    ScoreCard scoreCard;
+    Cards[] cardsOnTable;
+    int roundNo;
+    int turnOfPlayer;
+
     // constructors
-    public CardGame( Player p1, Player p2, Player p3, Player p4)
-    {
+    public CardGame(Player p1, Player p2, Player p3, Player p4) {
         players = new ArrayList<Player>();
-        ScoreCard scoreCard = new ScoreCard(4);
-        roundNo = 0;
+        this.scoreCard = new ScoreCard(4);
+        this.roundNo = 1;
         players.add(p1);
         players.add(p2);
         players.add(p3);
         players.add(p4);
+
+        fullPack = new Cards(true);
+        fullPack.shuffle();
+
+        int cardsPerPlayer = 5;
+        for (int i = 0; i < cardsPerPlayer; i++) {
+            for (Player pl : players) {
+                Card c = fullPack.getTopCard();
+                if (c != null) {
+                    pl.add(c);
+                }
+            }
+        }
+        this.turnOfPlayer = 0;
     }
-    
+
     // methods
-    public boolean playTurn( Player p, Card c)
-    {
-        if(p.playCard() == c){
+    public boolean playTurn(Player p, Card c) {
+        if (players.get(turnOfPlayer).equals(p)) {
+            turnOfPlayer = (turnOfPlayer + 1) % players.size();
+            if (turnOfPlayer == 0) {
+                roundNo++;
+                scoreCard.update(turnOfPlayer, c.getFaceValue());
+            }
             return true;
+        } else {
+            System.out.println("It's not " + p.getName() + "'s turn!");
+            return false;
         }
-        return false;
     }
-    
-    public boolean isTurnOf( Player p)
-    {
-        if(p.getName().equals(getName(getTurnOfPlayerNo()))){
-            return true;
-        }
-        return false;
+
+    public boolean isTurnOf(Player p) {
+        return players.get(turnOfPlayer).equals(p);
     }
-    
-    public boolean isGameOver()
-    {
+
+    public boolean isGameOver() {
         boolean gameOver = false;
 
-        for(Player pl : players){
-            if(pl.playCard() == null){
+        for (Player pl : players) {
+            if (pl.playCard() == null) {
                 gameOver = true;
             }
         }
         return gameOver;
     }
-    
-    public int getScore( int playerNumber)
-    {
+
+    public int getScore(int playerNumber) {
         return scoreCard.getScore(playerNumber);
     }
-    
-    public String getName( int playerNumber)
-    {
-        boolean done = false;
-        int index = 0;
-        while(!done){
-            int currentPlayerNumber = players.get(index).getName().charAt(1);
-            if(currentPlayerNumber == playerNumber){
-                return players.get(index).getName();
-            }
-            index++;
-            if(index > players.size()){
-                done = true;
-            }
+
+    public String getName(int playerNumber) {
+        if (playerNumber >= 0 && playerNumber < players.size()) {
+            return players.get(playerNumber).getName();
         }
         return "Not yet implemented";
     }
-    
-    public int getRoundNo()
-    {
+
+    public int getRoundNo() {
         return roundNo;
     }
-    
-    public int getTurnOfPlayerNo()
-    {
+
+    public int getTurnOfPlayerNo() {
         return turnOfPlayer;
     }
-    
-    public Player[] getWinners()
-    {
-        //if the player is winner than it have 1 for its index at int[] winnerArray
-        int[] winnerScores = scoreCard.getWinners();
-        Player[] winners;
 
-        for(int index = 0; index < players.size(); index++){
-            if(winnerScores[index] == 1){
-                winners[index] = players.get(index);
+    public Player[] getWinners() {
+        int[] winnerScores = scoreCard.getWinners();
+
+        int count = 0;
+        for (int score : winnerScores) {
+            if (score == 1)
+                count++;
+        }
+
+        Player[] winners = new Player[count];
+
+        int idx = 0;
+        for (int i = 0; i < players.size(); i++) {
+            if (winnerScores[i] == 1) {
+                winners[idx] = players.get(i);
+                idx++;
             }
         }
+
         return winners;
     }
-    
-    public String showScoreCard()
-    {
+
+    public String showScoreCard() {
         return scoreCard.toString();
     }
 }
+
